617144cc5e9b64a71ee3c32c0bcebe73be2f3b94
mrwhiskers <jamiemint4096@gmail.com>
2021-10-21 17:42:33 -0600

    runs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+import { provide } from '@vue/composition-api'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
   render: h => h(App),
 }).$mount('#app')
